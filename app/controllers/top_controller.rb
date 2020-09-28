6884c9e0fcbe53dffc764a59eadf67ce620cb16c
ryousuke-515 <ryo_suke515@icloud.com>
2020-09-28 14:42:28 +0900

    保存が成功した場合はトップページへ遷移する条件分岐

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,9 +1,23 @@
 class TopController < ApplicationController
   def index
-    @items = Item.all
+    @items = Item.order("create_at DESC")
   end
   
   def new
-    @item = Item.all
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    end
+  else
+    render :new
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :image, :description, :category_id, :price)
   end
 end
